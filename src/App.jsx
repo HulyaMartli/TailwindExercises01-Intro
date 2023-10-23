7a58764b5a7f562286c992adc59e695eef0d4593
Hulya Martli aka. Guerillagi <66822245+HulyaMartli@users.noreply.github.com>
2023-10-23 21:36:28 +0300

    font and color arrangements

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import './App.css'
 function App() {
 
   return (
-    <h1 className='text-3xl font-bold'>Hello World</h1>
+    <div className='container'>
+    <h1 className='text-3xl font-bold font-gemunu text-gega-red'>Hello World</h1>
+    </div>
   )
 }
 
