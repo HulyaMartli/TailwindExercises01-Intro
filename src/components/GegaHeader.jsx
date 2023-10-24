bed86631bfc0566cbc10fd9d04da9a65ecef12c9
Hulya Martli aka. Guerillagi <66822245+HulyaMartli@users.noreply.github.com>
2023-10-24 06:59:14 +0300

    header component completed

--- src/components/GegaHeader.jsx
+++ src/components/GegaHeader.jsx
@@ -3,7 +3,7 @@ export default function GegaHeader() {
         <header className="bg-black py-12 text-gega-grey font-gemunu uppercase">
             <div className="container flex items-center justify-between space-x-16">
                 {/* LOGO */}
-                <a href="#" className="text-6xl font-bold">GEGA</a>
+                <a href="#" className="text-6xl font-bold ms-4 text-transparent bg-gradient-to-r bg-clip-text from-gega-red to-gega-grey">GEGA</a>
 
                 {/* NAVIGATION */}
                 <nav className="flex justify-between flex-1">
@@ -17,14 +17,16 @@ export default function GegaHeader() {
                     </div>
 
                     {/* LOGIN AREA */}
-                    <div className="flex items-center space-x-8">
+                    <div className="flex items-center">
                         {/* SEARCH BAR */}
-                        <form>
-                            <div className="group border-r px-4 mx-4 py-1 border-gega-red">
-                                <input type="text" className="bg-transparent border-b border-gega-red focus:outline-none w-44"/>
-                                <button><i class="fa-solid fa-magnifying-glass"></i></button>
-                            </div>
+                        <form className="group border-r px-4 mx-4 py-1 border-gega-red">
+                            <input type="search" className="opacity-0 group-hover:opacity-100 bg-transparent border-b border-gega-red focus:outline-none  w-44 transition duration-500 cursor-pointer" />
+                            <button className="-ml-4 me-4"><i className="fa-solid fa-magnifying-glass group-hover:text-gega-red transition duration-500"></i></button>
                         </form>
+                        <div className="text-lg mx-4 space-x-4 ">
+                            <a href="#" className="hover:text-gega-melon transition duration-500">SIGN UP</a>
+                            <button className="bg-gega-red rounded px-3 py-1 hover:bg-gega-melon hover:text-black transition duration-500">LOGIN</button>
+                        </div>
                     </div>
                 </nav>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ export default {
   theme: {
     container: {
       center: true,
+      screens: {
+        'lg': '1140px',
+        'xl': '1140px',
+        '2xl': '1140px',
+      },
     },
 
     extend: {
